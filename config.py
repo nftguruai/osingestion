ea946460b3ddb1c5f44e4bec203060c43d67b3a4
nftguruai <samwal@samwals-mbp.myfiosgateway.com>
2022-06-11 19:48:56 -0400

    updated pg connections

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # Make sure to modify these settings before running the ingestion script!
 
-DB_NAME="tsdb"
-DB_HOST="something.tsdb.cloud.timescale.com"
-DB_USER="tsdbadmin"
-DB_PASS="passwd"
-DB_PORT="66666"
+DB_NAME="postgres"
+DB_HOST="localhost"
+DB_USER="postgres"
+DB_PASS="samwal74"
+DB_PORT="5432"
 OPENSEA_START_DATE="2021-10-01T00:00:00"
 OPENSEA_END_DATE="2021-10-06T23:59:59"
 OPENSEA_APIKEY="" # need to request from OpenSea's docs
